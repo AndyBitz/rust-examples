14eef58a31b1a218a9ca922583161045bc3b5040
Andy Bitz <artzbitz13+git@gmail.com>
2017-11-12 21:47:57 +0100

    Removed target folder from fitness

--- fitness/target/debug/fitness-e0ec0993e62eca81.d
+++
@@ -1,1 +0,0 @@
-D:\Users\andy\Projects\code\rust-examples\fitness\target\debug\fitness-e0ec0993e62eca81.exe: D:\Users\andy\Projects\code\rust-examples\fitness\src\main.rs
--- fitness/target/debug/fitness.d
+++
@@ -1,1 +0,0 @@
-D:\Users\andy\Projects\code\rust-examples\fitness\target\debug\fitness.exe: D:\Users\andy\Projects\code\rust-examples\fitness\src\main.rs
